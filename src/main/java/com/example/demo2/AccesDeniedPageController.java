46ee6731cb55e1d7e40e2514ca2dfc7654757380
MItsuki Shishido <169013117+dropmode@users.noreply.github.com>
2024-06-18 14:43:26 +0900

    新規作成

---
+++ src/main/java/com/example/demo2/AccesDeniedPageController.java
@@ -0,0 +1,14 @@
+package com.example.demo2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AccesDeniedPageController {
+
+	@GetMapping("/accessDeniedPage")
+	public String accessDeniedPage() {
+		return "accessDeniedPage";
+	}
+	
+}
